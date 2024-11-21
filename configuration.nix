b05209154682e32a29a9c6c60ad30bc81964d9ed
ProfessionalMenace <vondracekadam00@gmail.com>
2024-11-21 22:28:37 +0100

    added back locales

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,21 @@
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "cs_CZ.UTF-8";
+      LC_IDENTIFICATION = "cs_CZ.UTF-8";
+      LC_MEASUREMENT = "cs_CZ.UTF-8";
+      LC_MONETARY = "cs_CZ.UTF-8";
+      LC_NAME = "cs_CZ.UTF-8";
+      LC_NUMERIC = "cs_CZ.UTF-8";
+      LC_PAPER = "cs_CZ.UTF-8";
+      LC_TELEPHONE = "cs_CZ.UTF-8";
+      LC_TIME = "cs_CZ.UTF-8";
+    };
+  };
+  
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
